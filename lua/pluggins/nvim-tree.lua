0b29ec7b9c03ebb4b94b673ecd32e18d4463a549
MediocreMitch <mediocremitch@gmail.com>
2024-11-13 13:46:32 -0600

    set nvim-tree settings

--- lua/pluggins/nvim-tree.lua
+++ lua/pluggins/nvim-tree.lua
@@ -1,12 +1,20 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	opts = {
+		disable_netrw = true,
 		filters = {
 			dotfiles = false,
 		},
 		renderer = {
+			hidden_display = "all",
 			icons = {
 				web_devicons = require("mini.icons").mock_nvim_web_devicons(),
+				show = {
+					hidden = true,
+				},
+			},
+			indent_markers = {
+				enable = true,
 			},
 		},
 	},
