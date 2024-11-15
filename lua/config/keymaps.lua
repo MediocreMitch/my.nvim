0135ac68b03c778bb9711dbdb0aa1298d0c9b6c4
MediocreMitch <mediocremitch@gmail.com>
2024-11-15 09:07:26 -0600

    Removed accidental second declaration of wildr menu

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,32 +10,3 @@ vim.keymap.set("n", "<leader>pv", function()
 	vim.cmd("NvimTreeOpen")
 	vim.cmd("NvimTreeFocus")
 end)
-
---
---
--- Wilder Setup
---
---
-
-local wilder = require("wilder")
-wilder.setup({ modes = { ":" } })
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer({
-		highlighter = wilder.basic_highlighter(),
-		left = { " ", wilder.popupmenu_devicons() },
-		right = { " ", wilder.popupmenu_scrollbar() },
-	})
-)
-
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer({
-		border = wilder.popupmenu_border_theme({
-			border = { "", " ", "", "", "", "", "", "" },
-		}),
-		spinner = wilder.popupmenu_spinner({
-			frames = { "", "", "", "" },
-		}),
-	})
-)
