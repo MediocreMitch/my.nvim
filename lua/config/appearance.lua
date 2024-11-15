abd4fe1499a0749b2795b7fe5a76e4108336a5bd
MediocreMitch <mediocremitch@gmail.com>
2024-11-15 09:03:43 -0600

    Finished basic Wilder Setup

--- lua/config/appearance.lua
+++ lua/config/appearance.lua
@@ -64,3 +64,27 @@ require("nvim-tree").setup({
 		},
 	},
 })
+
+--
+--
+-- Wilder Setup
+--
+--
+
+local wilder = require("wilder")
+wilder.setup({ modes = { ":" } })
+wilder.set_option(
+	"renderer",
+	wilder.popupmenu_renderer({
+		highlighter = wilder.basic_highlighter(),
+		left = { " ", wilder.popupmenu_devicons() },
+		right = { " ", wilder.popupmenu_scrollbar() },
+	})
+)
+
+wilder.set_option(
+	"renderer",
+	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+		border = { "", " ", "", "", "", "", "", "" },
+	}))
+)
